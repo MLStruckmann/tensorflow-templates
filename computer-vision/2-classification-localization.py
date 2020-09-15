ef4530f19041464cbc1ef893b24003f6d2896cf9
Chris Schmitz <christopherl.schmitz@gmail.com>
2020-09-15 19:11:01 +0200

    Delete redundant files

--- computer-vision/2-classification-localization.py
+++
@@ -1,7 +0,0 @@
-# Localization
-
-base_model = keras.applications.xception.Xception(weights="imagenet",include_top=False)
-avg = keras.layers.GlobalAveragePooling2D()(base_model.output)
-class_output = keras.layers.Dense(n_classes, activation="softmax")(avg)
-loc_output = keras.layers.Dense(4)(avg)
-model = keras.Model(inputs=base_model.input,outputs=[class_output, loc_output])
